cefd445eba1abeb2ff5e85f484ccc2add0ed9a50
brianthtsou <95939519+brianthtsou@users.noreply.github.com>
2024-01-08 22:19:30 -0800

    starting login page

--- venv/app.py
+++ venv/app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+from flask_login import LoginManager, UserMixin
 from dotenv import load_dotenv
 import os
 
@@ -14,5 +14,43 @@ load_dotenv()
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
 app.secret_key = os.getenv("SECRET_KEY")
 
+# Loginmanager for logging users in and out
+login_manager = LoginManager()
+login_manager.init_app(app)
 
-@app.route('/', methods=['GET'])
+# initialize sqlalchemy
+db = SQLAlchemy()
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(24), unique=True, nullable=False)
+    password = db.Column(db.String(24), unique=True, nullable=False)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+
+
+# # Loginmanager for logging users in and out
+# login_manager = LoginManager()
+# login_manager.init_app(app)
+
+
+# # Initialize app with extension
+# db.init_app(app)
+
+# # Create database within app context
+# with app.app_context():
+    # db.create_all()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)

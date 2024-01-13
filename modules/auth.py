506d4e997ad7199913ec876bac9686ead3432e90
brianthtsou <95939519+brianthtsou@users.noreply.github.com>
2024-01-12 17:58:17 -0800

    cleaned up imports

--- modules/auth.py
+++ modules/auth.py
@@ -1,10 +1,7 @@
-from flask import Flask, Blueprint, render_template, url_for, redirect, session
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-from flask_wtf import FlaskForm
+from flask import Blueprint, render_template, url_for, redirect
+from flask_login import login_user, login_required, logout_user
 from flask_bcrypt import Bcrypt
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Length, ValidationError
+
 
 from .db import db
 from .forms import RegisterForm, LoginForm
--- modules/views.py
+++ modules/views.py
@@ -1,17 +1,6 @@
-from flask import Flask, Blueprint, render_template, url_for, redirect, session
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-from flask_wtf import FlaskForm
-from flask_bcrypt import Bcrypt
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Length, ValidationError
-
-from .db import db
-from .forms import RegisterForm, LoginForm
-from .models import User
+from flask import Blueprint, render_template, url_for, redirect, session
 
 views = Blueprint('views', __name__)
-bcrypt = Bcrypt()
 
 @views.route('/')
 def index():

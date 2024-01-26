d995647cce1566aeb285275ed15cd6658b9fe24d
brianthtsou <95939519+brianthtsou@users.noreply.github.com>
2024-01-25 21:44:12 -0800

    added styling, links, centering

--- static/js/create_playlist_page.js
+++ static/js/create_playlist_page.js
@@ -3,37 +3,38 @@ const blank_desc = document.getElementById("blank-description");
 const surprise_desc = document.getElementById("surprise-description");
 const discovery_slider = document.getElementById("playlist-scope-slider");
 
-const discovery_slider_output = document.getElementById("playlist-scope-slider-output")
+const discovery_slider_output = document.getElementById(
+  "playlist-scope-slider-output"
+);
 const dict = {
-    1 : "Short Term",
-    2 : "Medium Term",
-    3 : "Long Term"
-}
+  1: "Short Term",
+  2: "Medium Term",
+  3: "Long Term",
+};
 
 function sliderOutput(that) {
-    discovery_slider_output.innerHTML = dict[Number(that.value)];
+  discovery_slider_output.innerHTML = dict[Number(that.value)];
 }
 
 function descriptionCheck(that) {
-    if (that.value == "discovery") {
-        discovery_desc.style.display = "block";
-        discovery_slider.style.display = "block";
-        discovery_slider_output.style.display = "block";
-        blank_desc.style.display = "none";
-        surprise_desc.style.display = "none";
-    }
-    else if (that.value == "blank"){
-        blank_desc.style.display = "block";
-        discovery_desc.style.display = "none";
-        discovery_slider.style.display = "none";
-        discovery_slider_output.style.display = "none";
-        surprise_desc.style.display = "none";
-    }
-    else {
-        surprise_desc.style.display = "block";
-        blank_desc.style.display = "none";
-        discovery_desc.style.display = "none";
-        discovery_slider.style.display = "none";
-        discovery_slider_output.style.display = "none";
-    }
+  if (that.value == "discovery") {
+    discovery_desc.style.display = "block";
+    discovery_slider.style.display = "block";
+    discovery_slider_output.style.display = "block";
+    discovery_slider.style.textAlign = "center";
+    blank_desc.style.display = "none";
+    surprise_desc.style.display = "none";
+  } else if (that.value == "blank") {
+    blank_desc.style.display = "block";
+    discovery_desc.style.display = "none";
+    discovery_slider.style.display = "none";
+    discovery_slider_output.style.display = "none";
+    surprise_desc.style.display = "none";
+  } else {
+    surprise_desc.style.display = "block";
+    blank_desc.style.display = "none";
+    discovery_desc.style.display = "none";
+    discovery_slider.style.display = "none";
+    discovery_slider_output.style.display = "none";
+  }
 }
